aa546255db46f57d8874b4652fdc97d8440e904c
DainiusMI <mental.def@gmail.com>
2022-10-30 15:55:52 +0200

    removed conversion to roman

--- JS/main.js
+++ JS/main.js
@@ -179,9 +179,6 @@ buttons.forEach(function(currentButton) {
                     }
                 break;
 
-                case "Conver to Roman" : 
-                    currentOperand = covertToRoman(displayScreen.innerText);
-                break
             }
 
         }
@@ -189,34 +186,3 @@ buttons.forEach(function(currentButton) {
         displayUpdate();
     })
 })
-
-
-function covertToRoman(num) {
-
-    let outcome = "";
-    const romanValues = {
-        M: 1000,
-        CM: 900,
-        D: 500,
-        CD: 400,
-        C: 100,
-        XC: 90,
-        L: 50,
-        XL: 40,
-        X: 10,
-        IX: 9,
-        V: 5,
-        IV: 4,
-        III: 3,
-        II: 2,
-        I: 1,
-    }
-
-    for (let key in romanValues) {
-        while (num >= romanValues[key]) {
-            num -= romanValues[key];
-            outcome += key;
-        }
-    }
-    return outcome;
-}
